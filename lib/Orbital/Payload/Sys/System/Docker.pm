272d1008bcb00584579cc4449c8408c2c7a616b9
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-12-28 18:18:11 -0500

    Check for /.dockerenv
    It appears that `/proc/1/cgroup` does not always work anymore because it
    does not contain the expected string:
    
    ```shell
    0::/
    ```

--- lib/Orbital/Payload/Sys/System/Docker.pm
+++ lib/Orbital/Payload/Sys/System/Docker.pm
@@ -5,9 +5,18 @@ package Orbital::Payload::Sys::System::Docker;
 use Mu;
 use Orbital::Transfer::Common::Setup;
 
-classmethod is_inside_docker() {
+classmethod _check_cgroup() {
 	my $cgroup = path('/proc/1/cgroup');
 	return -f $cgroup  && $cgroup->slurp_utf8 =~ m,/(lxc|docker)/[0-9a-f]{64},s;
+}
+
+classmethod _check_dockerenv() {
+	return -f path('/.dockerenv');
+}
+
+classmethod is_inside_docker() {
+	return $class->_check_dockerenv
+		|| $class->_check_cgroup;
 }
 
 1;
